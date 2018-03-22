79f1efdcef1cc1d36d73d40db74e57f78ae3f484
silvestrevivo <silvestrevivomillan@gmail.com>
2018-03-22 21:57:13 +0100

    new version compiled from react

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import '../assets/sass/style.scss'
-import App from './components/app'
-
-ReactDOM.render(<App />, document.getElementById('root'))
